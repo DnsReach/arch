6e34c07fdccaa39e2471edb9d51073e2592eee77
DnsReach <dns@reach.localdomain>
2022-02-26 18:20:47 -0300

    Arch

--- src/components/componentsetup.js
+++ src/components/componentsetup.js
@@ -1,45 +1,90 @@
-import React from "react";
-import WIFI from "./wifi-component/wifi-comp";
-import Part from "./particionamento-compo/part";
-import "./comp.css";
+import React from 'react'
+import WIFI from './wifi-component/wifi-comp'
+import Part from './particionamento-compo/part'
+import './comp.css'
 
 const Component = () => {
-  return (
-    <>
-      <article
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-        }}
-      >
-        <h2 className="tag-titl">ARCH LINUX INSTALL</h2>
-
-        <div className="keyboard-conf">
-          <ol>
-            <li>KEYBOARD CONFIG : </li>
-            <div className="text-desc">
-              <p
+    return (
+        <>
+            <article
                 style={{
-                  textAlign: "center",
-                  marginTop: "5px",
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
                 }}
-              >
-                loadkeys br-abnt2
-              </p>
-            </div>
-            <li style={{ marginTop: "30px" }}> WIFI CONNECT :</li>
-            <WIFI />
-            <p style={{ color: "yellow" }}>Use: timedate set-ntp true </p>
-            <li style={{ marginTop: "30px" }}>PARTICIONATION :</li>
-            <Part />
-            <li style={{ marginTop: "30px" }}>PACKAGES :</li>
-            we
-          </ol>
-        </div>
-      </article>
-    </>
-  );
-};
+            >
+                <h2 className="tag-titl">ARCH LINUX INSTALL</h2>
 
-export default Component;
+                <div className="keyboard-conf">
+                    <ol>
+                        <li>KEYBOARD CONFIG : </li>
+                        <div className="text-desc">
+                            <p
+                                style={{
+                                    textAlign: 'center',
+                                    marginTop: '5px',
+                                }}
+                            >
+                                loadkeys br-abnt2
+                            </p>
+                        </div>
+                        <li style={{ marginTop: '30px' }}> WIFI CONNECT :</li>
+                        <WIFI />
+                        <p style={{ color: 'yellow' }}>
+                            Use: timedate set-ntp true{' '}
+                        </p>
+                        <li style={{ marginTop: '30px' }}>PARTICIONATION :</li>
+                        <Part />
+                        <li style={{ marginTop: '30px' }}>PACKAGES :</li>
+                        <p
+                            style={{
+                                textAlign: 'center',
+                                fontSize: '18px',
+                                paddingRight: '10px',
+                                borderWidth: '2px',
+                            }}
+                        >
+                            pacstrap /mnt base base-devel git vim grub
+                            efibootmgr networkmanager linux-lts linux-firmware
+                        </p>
+                        <li style={{ marginTop: '30px' }}>UUID AND FSTAB:</li>
+                        <p
+                            style={{
+                                fontSize: '18px',
+                            }}
+                        >
+                            genfstab -U /mnt >> /mnt/etc/fstab arch-chroot /mnt
+                            arch-chroot /mnt
+                        </p>
+                        <li style={{ marginTop: '30px' }}>LOCALE:</li>
+                        <p
+                            style={{
+                                fontSize: '18px',
+                            }}
+                        >
+                            vim /etc/locale.gen vim
+                            <br /> /etc/locale.conf <br /> vim
+                            /etc/vconsole.conf
+                        </p>
+                        <li style={{ marginTop: '30px' }}>
+                            HOST - ROOT - REBOOT
+                        </li>
+                        <p
+                            style={{
+                                fontSize: '18px',
+                            }}
+                        >
+                            vim /etc/hostname
+                            <br /> /etc/hosts <br />
+                            passwd <br />
+                            exit
+                        </p>
+                        REBOOT
+                    </ol>
+                </div>
+            </article>
+        </>
+    )
+}
+
+export default Component
